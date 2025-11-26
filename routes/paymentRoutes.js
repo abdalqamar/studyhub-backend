e9389e15044a78a88f14d8166d3704e5299cd5a6
Abdal Qamar <abdalqamar10@gmail.com>
2025-11-27 04:27:52 +0530

    paymenets routes added

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { razorpayWebhook } from "../controllers/paymentController.js";
+import {
+  createOrder,
+  razorpayWebhook,
+} from "../controllers/paymentController.js";
 
 const router = express.Router();
+router.post("/order", createOrder);
 router.post(
   "/webhook",
   express.raw({ type: "application/json" }),
