e5647350edb04bfcdc80899769519f3c46196601
Abdal Qamar <abdalqamar10@gmail.com>
2026-01-05 13:41:17 +0530

    Resend email setup completed

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,34 +1,3 @@
-// import nodemailer from "nodemailer";
-
-// const sendEmail = async (email, subject, htmlBody) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.SMTP_HOST,
-//       port: Number(process.env.SMTP_PORT),
-//       secure: process.env.SMTP_PORT === "465",
-//       auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASS,
-//       },
-//     });
-
-//     const info = await transporter.sendMail({
-//       from: `"${process.env.FROM_NAME}" <${process.env.FROM_EMAIL}>`,
-//       to: email,
-//       subject,
-//       html: htmlBody,
-//     });
-
-//     console.log("Email sent:", info.messageId);
-//     return info;
-//   } catch (error) {
-//     console.error("Email send error:", error);
-//     throw new Error("Email could not be sent.");
-//   }
-// };
-
-// export default sendEmail;
-
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -47,7 +16,7 @@ const sendEmail = async (email, subject, htmlBody) => {
       throw new Error("Email could not be sent.");
     }
 
-    console.log("Email sent:", data?.id || "No ID returned");
+    console.log("Email sent:", data);
     return data;
   } catch (error) {
     console.error("Email send error:", error);
